68f05ae5bda86f403a8edc6a3752af8e64daa4f2
DuiWeL28 <42545152+DuiWeL28@users.noreply.github.com>
2018-09-07 03:03:56 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,4 +3,9 @@
 while read -r device || [[ -n $device ]]
 do
     add_lunch_combo "aoscp_$device-userdebug"
+    add_lunch_combo "lineageos_$device-n5110"
+    add_lunch_combo "LineageOS_$device-n5110"
+    add_lunch_combo "DuiWeL28_$device-n5110"
+    add_lunch_combo "samsung_$device-n5110"
+    
 done < vendor/aoscp/aoscp.devices
